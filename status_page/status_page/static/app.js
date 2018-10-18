3ab466f548a585cd15d216f6cf66c7d36d06b00e
Sean Dague <sean@dague.net>
2018-10-18 11:20:43 -0400

    add new styling

--- status_page/status_page/static/app.js
+++ status_page/status_page/static/app.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
         var services = data.services;
         servicesStatus.empty();
         servicesStatus.append("<table>");
-        servicesStatus.append("<tr><th>Service</th><th>Status</th><th>Last Updated</th><th>Comments</th></tr>");
+        servicesStatus.append("<tr class='heading'><th>Service</th><th>Status</th><th>Last Updated</th><th>Comments</th></tr>");
 
         for (var i = 0; i < services.length; i++) {
             var service = services[i];
 
-            servicesStatus.append("<tr>");
-            servicesStatus.append("<td>" + service.name + "</td>");
-            servicesStatus.append("<td>" + service.status + "</td>");
-            servicesStatus.append("<td>" + service.updated_at + "</td>");
-            servicesStatus.append("<td>" + service.msg + "</td>");
-            servicesStatus.append("</tr>");
+            var row = "<tr class='data'>";
+            row += "<td>" + service.name + "</td>";
+            row += "<td>" + service.status + "</td>";
+            row += "<td>" + service.updated_at + "</td>";
+            row += "<td>" + service.msg + "</td>";
+            row += "</tr>";
+            servicesStatus.append(row);
         }
         servicesStatus.append("</table>");
 
